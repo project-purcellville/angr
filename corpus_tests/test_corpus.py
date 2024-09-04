3deda3cc484dc206a6a9ee6002282ba4f1b15f2e
Doug Rogers <doug@karambit.ai>
2024-09-04 18:08:57 -0400

    Add support for multiple paths. Add support for pull_request events. Add scripts for debugging and running with 'gh act'.

--- corpus_tests/test_corpus.py
+++ corpus_tests/test_corpus.py
@@ -11,8 +11,10 @@ Invoke this test script with the `pytest --insta` switch to enable the snapshot 
 
 logging.basicConfig(level=logging.CRITICAL, force=True)
 
+SNAPSHOTS_REPO_BASE_URL = 'https://github.com/project-purcellville/snapshots-0000/'
 
-def analyze_binary(binary_path):
+
+def analyze_binary(binary_path: str) -> dict:
     """
     Run the binary through CFG generation and extract the decompilation from the Decompiler analysis.
     The intention of this analysis function is to use as little angr interfaces as possible since they may
@@ -25,6 +27,7 @@ def analyze_binary(binary_path):
     function: angr.knowledge_plugins.functions.function.Function
     for function in cfg.functions.values():
         function.normalize()
+        func_key = f"{function.addr}:{function.name}"
 
         # Wrapping in a try/except because the decompiler sometimes fails
         try:
@@ -43,10 +46,9 @@ def analyze_binary(binary_path):
                     ),
                 ],
             )
-        except Exception as e:
-            print(e)
-
-        func_key = f"{function.addr}:{function.name}"
+        except Exception as ex:
+            print(f'Exception decompiling "{func_key}()" in "{binary_path}":'
+                  f'\n{ex}\nContinuing with other functions.')
 
         if decomp.codegen:
             decompilation[func_key] = decomp.codegen.text
@@ -56,13 +58,78 @@ def analyze_binary(binary_path):
     return decompilation
 
 
-def test_functions_decompilation(binary, snapshot):
-    analysis = analyze_binary(binary)
-    snapshots_binary = re.sub('^binaries/', 'snapshots/', binary)
-    dir_path = os.path.dirname(snapshots_binary).replace('/', os.path.sep)
-    os.makedirs(dir_path, exist_ok=True)
-    snapshot_file_path = os.path.join(dir_path, f"{os.path.basename(binary)}.json")
-    print(f'-=-=-=-=-=-=> Snapshot name: "{snapshot_file_path}"')
-    with open(snapshot_file_path, "w") as f:
-        json.dump(analysis, f)
-    # assert snapshot(snapshot_file_path) == analysis
+def create_diffable_decompilation(decompiler_output: dict) -> str|None:
+    """
+    Convert the decompiler output `dict` into JSON, but also modify it to
+    allow easy diffing (appending actual newlines '\n' to escaped newlines
+    '\\n', etc.), returning the result as a string.
+    """
+    try:
+        decompiler_json = json.dumps(decompiler_output)
+        decompiler_json_newlined = re.sub('\\\\n', '\\\\n\n', decompiler_json)
+    except Exception as ex:
+        print(f'Exception converting decompiler output to newlined-JSON:\n{ex}')
+        return None
+    return decompiler_json_newlined
+
+
+def pytest_insta_snapshot_name(binary: str) -> str:
+    """
+    Return the undecorated snapshot name that `pytest-insta` will use to
+    retrieve the snapshot from local storage in `./snapshots/`. Note that this
+    will *not* contain the decorations that `pytest-insta` uses, like its
+    `"<test-name>__<case-name>__"` prefix and `"__<#>.<ext>"` suffix.
+    """
+    return re.sub('/', '_', re.sub('^binaries/', '', binary)) + ".json.txt"
+
+
+def write_decompilation_to_snapshot_dir(decompilation: str, binary: str) -> bool:
+    snapshot_file_path = re.sub('^binaries/', 'snapshots/', binary) + '.json.txt'
+    try:
+        with open(snapshot_file_path, 'w') as f:
+            f.write(decompilation)
+    except Exception as ex:
+        print(f'Exception writing decompilation to "{snapshot_file_path}":\n{ex}')
+        return False
+    return True
+
+
+def test_decompilation(binary, snapshot):
+    """
+    In order to accommodate insta's need to have snapshots stored in a single
+    file directly in the local `./snapshots/` directory, but also allow a
+    reasonable comparison with the snapshots repo using github's pull request
+    comparison, we pull down each snapshot from deeper within the snapshot
+    repo's directory structure, but place it with a flat name (path delimiters
+    replaced) in the `./snapshots/`, tweaking it to work with the standard
+    way `pytest-insta` works (see `pytest_insta_snapshot_name()` above).
+
+    Note that the snapshot should already be downloaded (by the workflow or
+    manually) and placed in the snapshots directory. The name should be
+    'corpus__decompilation__<binary-subpath-/-escaped>.json.txt__0.txt'. For
+    example, the binary 'binaries/my/path/binary.exe' should be named
+    'corpus__decompilation__my_path_binary.exe.json.txt__0.txt' in the
+    local `./snapshots/` directory.
+
+    This needs to stay in sync with the code that downloads the snapshots
+    in `corpus_test.yml`.
+    """
+    decompilation = analyze_binary(binary)
+    if not decompilation:
+        # Message already emitted.
+        return False
+
+    diffable_decompilation = create_diffable_decompilation(decompilation)
+
+    # Currently this is being done by the github action workflow so that this
+    # test does not need to do any network operations:
+    # snapshot_name = retrieve_remote_snapshot(binary)
+    # if not snapshot_name:
+    #     # Message already emitted.
+    #     return False
+
+    # This 
+    snapshot_name = pytest_insta_snapshot_name(binary)
+    # write_decompilation_to_snapshot_dir(decompilation, binary)
+    print(f'Loading snapshot "{snapshot_name}".')
+    assert snapshot(snapshot_name) == diffable_decompilation
